31c0559190f88cde86d53405152ffb360d276589
Psycho Mods <nishsin26@gmail.com>
2018-01-03 18:14:20 +0530

    Update PsychoTweaks.java

--- src/com/android/settings/Psycho/PsychoTweaks.java
+++ src/com/android/settings/Psycho/PsychoTweaks.java
@@ -12,7 +12,7 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-public class dotSettings extends SettingsPreferenceFragment implements
+public class PsychoTweaks extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "PsychoTweaks";
 
