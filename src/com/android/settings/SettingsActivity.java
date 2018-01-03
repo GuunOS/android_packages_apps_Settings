a8f78df58f6ba6a3494c8502c08859bf24a68e8c
Psycho Mods <nishsin26@gmail.com>
2018-01-03 17:57:59 +0530

    Update SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -379,7 +379,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             MasterClear.class.getName(),
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
-            AutomaticStorageManagerSettings.class.getName()
+            AutomaticStorageManagerSettings.class.getName(),
+            PsychoTweaks.class.getName()
     };
 
 
