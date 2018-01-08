9d34c356adb5018612ee7bb7548a51bd7d693842
Psycho Mods <nishsin26@gmail.com>
2018-01-09 00:18:08 +0530

    Update Buttons.java

--- src/com/android/settings/fragments/Buttons.java
+++ src/com/android/settings/fragments/Buttons.java
@@ -12,7 +12,7 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-public class PsychoTweaks extends SettingsPreferenceFragment implements
+public class Buttons extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "ButtonsTweaks";
 
