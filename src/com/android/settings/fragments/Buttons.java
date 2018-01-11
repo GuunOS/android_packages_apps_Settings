116e5eea0cf13b67ccf7a746a60ae1524dc57228
Psycho Mods <nishsin26@gmail.com>
2018-01-11 11:48:44 +0530

    Call cmParts Buttons Settings

--- src/com/android/settings/fragments/Buttons.java
+++ src/com/android/settings/fragments/Buttons.java
@@ -5,6 +5,10 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.content.ComponentName;
+import android.content.Intent;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
@@ -26,6 +30,27 @@ public class Buttons extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.buttons);
+            
+        Preference navpreference = getPreferenceManager().findPreference(getString(R.string.buttons_category));
+        navpreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+        	public boolean onPreferenceClick(Preference navpreference) {
+		
+		final Intent intent = new Intent(Intent.ACTION_MAIN, null);
+
+		intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+		final ComponentName cn = new ComponentName("org.cyanogenmod.cmparts", "org.cyanogenmod.cmparts.input.ButtonSettings");
+
+		intent.setComponent(cn);
+
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+		startActivity( intent);
+
+		return true;
+        	}
+        });
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
