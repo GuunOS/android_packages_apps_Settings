e0a59629df0c184f3762cfeed551b0e6d08475b1
Psycho Mods <nishsin26@gmail.com>
2018-01-19 13:43:25 +0530

    Update Statusbar.java

--- src/com/android/settings/fragments/Statusbar.java
+++ src/com/android/settings/fragments/Statusbar.java
@@ -1,17 +1,16 @@
 package com.android.settings.fragments;
 
+
 import android.os.Bundle;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.content.pm.PackageManager;
 import android.preference.SwitchPreference;
 
-import com.android.internal.logging.MetricsProto.MetricsEvent;
-
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 public class Statusbar extends PreferenceFragment implements
@@ -20,11 +19,6 @@ public class Statusbar extends PreferenceFragment implements
     private static final String KEY_SHOW_FOURG = "show_fourg";
 
     private SwitchPreference mShowFourG;
-
-    @Override
-    protected int getMetricsCategory() {
-        return MetricsEvent.APPLICATION;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
