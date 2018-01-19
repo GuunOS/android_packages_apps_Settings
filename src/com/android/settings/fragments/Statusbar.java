736f0566a20339b7e69474381abd8c76fdf62128
Psycho-Mods <nishsin26@gmail.com>
2018-01-19 19:41:15 +0530

    Revert "Tweaks: Show 4G instead of LTE switch [2/2]"
    This reverts commit 5fd3725df1fa92f09f8e19104b2c1afa5dc4e048.
    
    Derped

--- src/com/android/settings/fragments/Statusbar.java
+++ src/com/android/settings/fragments/Statusbar.java
@@ -5,8 +5,6 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
-import android.content.pm.PackageManager;
-import android.preference.SwitchPreference;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
@@ -17,9 +15,6 @@ import com.android.settings.Utils;
 public class Statusbar extends PreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "StatusbarTweaks";
-    private static final String KEY_SHOW_FOURG = "show_fourg";
-
-    private SwitchPreference mShowFourG;
 
     @Override
     protected int getMetricsCategory() {
@@ -31,13 +26,6 @@ public class Statusbar extends PreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.statusbar);
-
-        PreferenceScreen prefSet = getPreferenceScreen();
-        mShowFourG = (SwitchPreference) prefSet.findPreference(KEY_SHOW_FOURG);
-            PackageManager pm = getActivity().getPackageManager();
-            if (!pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
-                prefSet.removePreference(mShowFourG);
-            }
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
