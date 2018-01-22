a56ee190d4d2d00ee3d4487b61ee2146df042874
Psycho Mods <nishsin26@gmail.com>
2018-01-22 09:56:21 +0530

    Update PsychoTweaks.java

--- src/com/android/settings/Psycho/PsychoTweaks.java
+++ src/com/android/settings/Psycho/PsychoTweaks.java
@@ -2,15 +2,24 @@ package com.android.settings.Psycho;
 
 import android.os.Bundle;
 import android.preference.Preference;
-import android.preference.PreferenceFragment;
-import android.preference.Preference.OnPreferenceClickListener;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.content.ComponentName;
-import android.content.Intent;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
-public class PsychoTweaks extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+public class PsychoTweaks extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "PsychoTweaks";
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.APPLICATION;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,8 +28,8 @@ public class PsychoTweaks extends PreferenceFragment implements Preference.OnPre
         addPreferencesFromResource(R.xml.psycho);
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object o) {
-        return false;
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+        return true;
     }
 }
