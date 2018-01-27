0635bc0cbfb9aec4c975db6ce66946e82c08f14c
Psycho-Mods <nishsin26@gmail.com>
2018-01-27 21:04:21 +0530

    Did something silly, but might work

--- src/com/android/settings/fragments/Statusbar.java
+++ src/com/android/settings/fragments/Statusbar.java
@@ -40,6 +40,27 @@ public class Statusbar extends PreferenceFragment implements Preference.OnPrefer
         	}
         });
 
+    Preference network = getPreferenceManager().findPreference("nettraffic_settings");
+    network.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+      public boolean onPreferenceClick(Preference network) {
+
+    final Intent intent = new Intent(Intent.ACTION_MAIN, null);
+
+    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+    final ComponentName cn = new ComponentName("com.android.settings", "com.android.settings.fragments.NetworkTrafficSettings");
+
+    intent.setComponent(cn);
+
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+    startActivity( intent);
+
+    return true;
+      }
+    });
+
     }
 
     @Override
