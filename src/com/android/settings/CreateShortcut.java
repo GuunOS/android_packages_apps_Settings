5823e8c7d90e95abd565697aa108d7e9ac65fec9
Psycho-Mods <nishsin26@gmail.com>
2018-02-07 20:46:13 +0530

    Revert "Update CreateShortcut.java"
    This reverts commit d6d4238eaaeb9bf94a20c449a1806748a277e8d9.
    
    Bring Back Original Name

--- src/com/android/settings/CreateShortcut.java
+++ src/com/android/settings/CreateShortcut.java
@@ -53,7 +53,7 @@ public class CreateShortcut extends LauncherActivity {
         shortcutIntent.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
         Intent intent = new Intent();
         intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
-                Intent.ShortcutIconResource.fromContext(this, R.mipmap.ic_launcher));
+                Intent.ShortcutIconResource.fromContext(this, R.mipmap.ic_launcher_settings));
         intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
         intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, itemForPosition(position).label);
         ResolveInfo resolveInfo = itemForPosition(position).resolveInfo;
