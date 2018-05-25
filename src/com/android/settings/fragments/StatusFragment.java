4927d92b068eebd6fec7344bb0d535c435270167
Psycho-Mods <nishsin26@gmail.com>
2018-05-25 21:40:03 +0530

    Network Traffic

--- src/com/android/settings/fragments/StatusFragment.java
+++ src/com/android/settings/fragments/StatusFragment.java
@@ -1,17 +1,16 @@
 package com.android.settings.fragments;
 
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceScreen;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceActivity;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
-
 
 public class StatusFragment extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
