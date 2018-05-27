251e33566978557fc9d633b33687a4b950dbebb1
Psycho-Mods <nishsin26@gmail.com>
2018-05-27 14:45:14 +0530

    Bring Up Accidental Touch

--- src/com/android/settings/fragments/DisplayFragment.java
+++ src/com/android/settings/fragments/DisplayFragment.java
@@ -5,6 +5,13 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.content.Context;
+import android.preference.SwitchPreference;
+import android.content.res.Resources;
+import android.content.ContentResolver;
+import android.provider.Settings;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
@@ -16,6 +23,13 @@ public class DisplayFragment extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "DisplayTweaks";
 
+    private int mDeviceHardwareKeys;
+    private static final String KEY_ANBI = "anbi_enabled";
+
+    private SwitchPreference mAnbiPreference;
+
+    private SwitchPreference mAnbiPreference;
+
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.APPLICATION;
@@ -26,6 +40,17 @@ public class DisplayFragment extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.display);
+
+        Resources res = getActivity().getResources();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mDeviceHardwareKeys = res.getInteger(
+                                    com.android.internal.R.integer.config_deviceHardwareKeys);
+
+        mAnbiPreference = (SwitchPreference) findPreference(KEY_ANBI);
+
+        if (mDeviceHardwareKeys == 0)
+          prefScreen.removePreference(mAnbiPreference);
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
