216c525eadaa23563dade8ab8e1fbf61682f0902
Psycho-Mods <nishsin26@gmail.com>
2018-06-02 12:16:53 +0530

    Improved some logics

--- src/com/android/settings/fragments/DisplayFragment.java
+++ src/com/android/settings/fragments/DisplayFragment.java
@@ -1,39 +1,22 @@
 package com.android.settings.fragments;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.admin.DevicePolicyManager;
-import android.content.ActivityNotFoundException;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.ContentResolver;
 import android.content.res.Resources;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.Uri;
-import android.nfc.NfcAdapter;
-import android.nfc.NfcManager;
 import android.os.Bundle;
-import android.os.SystemProperties;
 import android.os.UserHandle;
-import android.os.UserManager;
-import android.provider.SearchIndexableResource;
-import android.provider.Settings;
-import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
-import android.telephony.TelephonyManager;
-import android.text.TextUtils;
-import android.util.Log;
-
-import com.android.internal.logging.MetricsProto.MetricsEvent;
+import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
+import com.android.settings.preferences.SystemSettingSwitchPreference;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
 
 public class DisplayFragment extends SettingsPreferenceFragment implements
@@ -62,21 +45,25 @@ public class DisplayFragment extends SettingsPreferenceFragment implements
         mDeviceHardwareKeys = res.getInteger(
                                     com.android.internal.R.integer.config_deviceHardwareKeys);
 
-        mAnbiPreference = (SwitchPreference) findPreference(KEY_ANBI);
+        mAnbiPreference = (SystemSettingSwitchPreference) findPreference(KEY_ANBI);
 
         if (mDeviceHardwareKeys == 0)
           prefScreen.removePreference(mAnbiPreference);
     }
 
     @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
+    public void onResume() {
+        super.onResume();
+    }
 
-        // Let the intents be launched by the Preference manager
-        return super.onPreferenceTreeClick(preference);
+    @Override
+    public void onPause() {
+        super.onPause();
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+        final String key = preference.getKey();
         return true;
     }
-}
+
+  }
--- src/com/android/settings/fragments/StatusFragment.java
+++ src/com/android/settings/fragments/StatusFragment.java
@@ -1,44 +1,54 @@
+/*
+ * Copyright (C) 2016 CarbonROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.settings.fragments;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.admin.DevicePolicyManager;
-import android.content.ActivityNotFoundException;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.ContentResolver;
 import android.content.res.Resources;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.Uri;
-import android.nfc.NfcAdapter;
-import android.nfc.NfcManager;
 import android.os.Bundle;
-import android.os.SystemProperties;
 import android.os.UserHandle;
-import android.os.UserManager;
-import android.provider.SearchIndexableResource;
-import android.provider.Settings;
-import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
-import android.telephony.TelephonyManager;
-import android.text.TextUtils;
-import android.util.Log;
-
-import com.android.internal.logging.MetricsProto.MetricsEvent;
+import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
+import com.android.settings.preferences.SystemSettingSwitchPreference;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
 
-public class StatusFragment extends SettingsPreferenceFragment implements
+public class Statusbar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
-    private static final String TAG = "StatusBarTweaks";
+    private static final String TAG = "Statusbar";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.statusbar);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+    }
 
     @Override
     protected int getMetricsCategory() {
@@ -46,20 +56,18 @@ public class StatusFragment extends SettingsPreferenceFragment implements
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.statusbar);
+    public void onResume() {
+        super.onResume();
     }
 
     @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-
-        // Let the intents be launched by the Preference manager
-        return super.onPreferenceTreeClick(preference);
+    public void onPause() {
+        super.onPause();
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
         return true;
     }
+
 }
